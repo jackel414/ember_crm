b519b3cc61df345e379df1ff27b20898c301dad4
Zachary Mays <zackmays@gmail.com>
2015-01-05 17:29:25 -0800

    Initial Commit

---
+++ app/assets/javascripts/store.js
@@ -0,0 +1,22 @@
+DS.RESTAdapter.reopen({
+  namespace: 'api/v1'
+})
+
+App.Store = Ember.store.extend({})
+
+// Override the default adapter with the `DS.ActiveModelAdapter` which
+// is built to work nicely with the ActiveModel::Serializers gem.
+App.ApplicationAdapter = DS.ActiveModelAdapter.extend({
+
+});
+
+
+// Adds X-CSRF-Token to all REST requests.
+// Allows for the use of Rails protect_from_forgery
+// The CSRF Token is normally found in app/views/layouts/application.html.*
+// inserted with the rails helper: "csrf_meta_tags"
+DS.RESTAdapter.reopen({
+  headers: {
+    "X-CSRF-Token": $('meta[name="csrf-token"]').attr('content')
+  }
+});
