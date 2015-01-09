0f91f2b44d2178743ddd9d0e25e5ec278d7102ab
Zachary Mays <zackmays@gmail.com>
2015-01-08 17:18:16 -0800

    Allow leads to be edited

---
+++ app/assets/javascripts/controllers/lead.js
@@ -0,0 +1,13 @@
+App.LeadController = Ember.ObjectController.extend({
+
+	isEditing: false,
+  showUnsavedMessage: function() {
+    return this.get('isDirty') && !this.get('isSaving')
+  }.property('isDirty', 'isSaving'),
+  actions: {
+    saveChanges: function() {
+      this.get('model').save();
+    }
+  }
+
+});
